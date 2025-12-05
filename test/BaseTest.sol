acf262f1bdecdaa2f7f7b618b189b6df03ed89e2
A.L. <xenide@users.noreply.github.com>
2025-12-05 14:17:35 -0300

    test: setup the base

---
+++ test/BaseTest.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test } from "forge-std/Test.sol";
+
+contract BaseTest is Test {
+
+    function setUp() public {
+    }
+
+    function test_Increment() public {
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+    }
+}
