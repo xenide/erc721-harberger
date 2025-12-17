a53364f95cc7cf11deffeffe953123d10fd457da
A.L. <xenide@users.noreply.github.com>
2025-12-17 15:56:38 -0300

    fix: slither issues

--- src/ERC721Harberger.sol
+++ src/ERC721Harberger.sol
@@ -83,8 +83,8 @@ contract ERC721Harberger is IERC721Harberger, ERC721, Ownable, ReentrancyGuardTr
         require(aNewPrice >= Constants.MIN_NFT_PRICE, Errors.NFTPriceTooLow());
         require(aNewPrice <= Constants.MAX_SUPPORTED_PRICE, Errors.NFTPriceTooHigh());
 
-        uint256 lRemainingTaxCredit;
-        uint256 lGracePeriodPenalty;
+        uint256 lRemainingTaxCredit = 0;
+        uint256 lGracePeriodPenalty = 0;
         // happy case: NFT is in a tax compliant state, and not in a grace period state
         if (!isInGracePeriod(aTokenId)) {
             lRemainingTaxCredit = _taxInfo[aTokenId].lastPaidAmt * (taxEpochEnd(aTokenId) - block.timestamp)
@@ -157,8 +157,7 @@ contract ERC721Harberger is IERC721Harberger, ERC721, Ownable, ReentrancyGuardTr
         uint256 lPrice;
         uint256 lAuctionPeriodEnd = _gracePeriodEnd(aTokenId) + Constants.TAX_EPOCH_DURATION;
 
-        uint256 lTotalDue;
-        uint256 lTotalRefundPrevOwner;
+        uint256 lTotalRefundPrevOwner = 0;
 
         // Case 1: Buy during tax epoch and grace period
         if (block.timestamp <= _gracePeriodEnd(aTokenId)) {
@@ -189,7 +188,7 @@ contract ERC721Harberger is IERC721Harberger, ERC721, Ownable, ReentrancyGuardTr
         }
 
         uint256 lTaxes = _calcTaxDue(lPrice);
-        lTotalDue = lPrice + lTaxes;
+        uint256 lTotalDue = lPrice + lTaxes;
         require(lTotalDue <= aMaxPriceIncludingTaxes, Errors.MaxPriceIncludingTaxesExceeded());
         _pull(msg.sender, lTotalDue);
         _updateTaxInfo(aTokenId, lPrice, lTaxes);
